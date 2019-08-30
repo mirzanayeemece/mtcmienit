51af74ab2e190b27f8c3b96f511c708b3ba5ebf9
Farhan Khan <farha100669@gmail.com>
2019-08-30 17:48:00 +0600

    routes added for admin->admin

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,11 @@ Route::post('/update_venue/{id}','HomeController@update_venue');
 
 Route::get('/training/venueRes', 'HomeController@venueRes')->name('venueRes');
 Route::get('/training/venueAlloc', 'HomeController@venueAlloc')->name('venueAlloc');
+
+//user
+Route::get('/admin/user', 'HomeController@user')->name('user');
+Route::get('/admin/adduser', 'HomeController@adduser')->name('add_user');
+Route::post('/saveuser','HomeController@saveuser');
+Route::get('/delete_user/{id}','HomeController@delete_user');
+Route::get('/edit_user/{id}','HomeController@edit_user')->name('edit_user');
+Route::post('/update_user/{id}','HomeController@update_user');
