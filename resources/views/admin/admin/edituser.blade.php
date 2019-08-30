3abc528e4489b98faefb7d22762a6de2aed7d0aa
Farhan Khan <farha100669@gmail.com>
2019-08-30 17:31:12 +0600

    added admin folder inside resources->views->admin

---
+++ resources/views/admin/admin/edituser.blade.php
@@ -0,0 +1,80 @@
+@extends('admin.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header"> <big> <strong>EDIT USER</strong> </big> </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <a href="{{URL::to('/admin/user')}}" class="btn btn-primary">Back</a>
+                </div>
+   
+   @if (count($errors) > 0)
+    <div class="alert alert-danger" style="padding: 0 30px 0 30px;">
+     Upload Validation Error<br><br>
+     <ul>
+      @foreach ($errors->all() as $error)
+       <li>{{ $error }}</li>
+      @endforeach
+     </ul>
+    </div>
+   @endif
+   @if ($message = Session::get('success'))
+   <div class="alert alert-success alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+           <strong>{{ $message }}</strong>
+   </div>
+   @endif
+
+  <!--form-->
+ 
+  <form class="" action="{{ url('/update_user',$all_users_info->id) }}" method="post" enctype="multipart/form-data" style="padding: 0 30px 0 30px;">
+    {{ csrf_field() }}    
+
+    <p class="alert-success" style="font-size: 20px; color: white; background:#149278; padding: 0 30px 0 30px;">
+        @php
+          $message=Session::get('message');
+            if($message) {
+              echo $message;
+              Session::put('message',null);
+            }
+        @endphp
+            
+      </p>
+
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" value="{{$all_users_info->name}}" id="name" name="name" required>
+      </div>
+
+      <div class="form-group">
+        <label for="location">Email:</label>
+        <input type="text" class="form-control" value="{{$all_users_info->email}}" id="email" name="email" required>
+      </div>
+
+      
+
+      
+      
+
+        <!-- Button -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-success" >Update</button>
+        </div>
+
+        
+    </form>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
